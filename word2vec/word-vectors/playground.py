edd3f71fe1bb430d46dee14a311ac7bf49b54530
sempod <vaclavgregor17@gmail.com>
2024-03-29 18:21:07 +0100

    Some word vector examples.

--- word2vec/word-vectors/playground.py
+++ word2vec/word-vectors/playground.py
@@ -2,29 +2,29 @@ from __future__ import unicode_literals
 import spacy 
 import numpy as np 
 
-# charge les vecteurs dans notre modèle (nlp) 
+# load the word vectors into our model 
 nlp = spacy.load("en_core_web_lg")
-# lit le texte (joyland.txt) et crée ainsi notre corpus (doc) 
+# read the text (joyland.txt) and create our corpus (doc)
 doc = nlp(open("joyland.txt").read())
 
-# garde que les mots alphabétiques et les convertit en minuscule 
+# keep only alphabetical words and lowercase them 
 tokens = list(set([w.text.lower() for w in doc if w.is_alpha]))
-# garde que les mots qui ont un vecteur dans notre modèle 
+# keep only words that have a vector in our model 
 tokens = list(filter(lambda x: nlp.vocab.has_vector(x), tokens))
-# extrait les phrases 
+# extract sentences
 sentences = list(doc.sents)
 
-""" Calcule la similarité cosinus entre deux vecteurs. """
+""" Calculates the cosine similarity between two vectors. """
 def cos_sim(u, v):
   return np.dot(u, v)/np.linalg.norm(u)/np.linalg.norm(v)
 
-""" Retourne le vecteur correspondant au mot donné en entrée. """
+""" Returns the vector corresponding to the input string. """
 def vec(s : str):
   return np.array(nlp.vocab[s].vector)
 
-""" Retourne le mot correspondant au vecteur donné en entrée, 
-en cherchant dans le modèle le vecteur qui est le plus près de
-l'entrée. """
+""" Return the word corresponding to the given vector by looking 
+for the closest vector in the model.
+"""
 def word(v):
   max_sim = -1 
   res = ""
@@ -36,13 +36,13 @@ def word(v):
         res = w.text
   return res  
 
-""" Retourne la liste des mots les plus similaires au mot en entrée. """
+""" Return the list of most similar words to the given one. """
 def closest_words(token_list, v, n=5):
   return sorted(token_list, 
                    key=lambda x: cos_sim(vec(x), v), 
                    reverse=True)[:n]
 
-""" Retorne la liste des phrases les plus similaires à la phrase en entrée. """
+""" Return closest sentences """
 def closest_sents(sents_list, sent, n=5):
   return sorted(sents_list, 
                 key=lambda x: cos_sim(x.vector, nlp(sent).vector), 
